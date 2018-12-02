706a07237cf39c2f5373a078425bdd8c37971a13
Sarah Phillips <Sarah@Louies-MacBook-Pro.local>
2018-12-02 20:46:22 +0000

    Pie chart calendar works, accurate to dates

--- SocialGraph.py
+++ SocialGraph.py
@@ -3,40 +3,86 @@ import matplotlib.pyplot as plt
 import numpy as np
 import getpass
 import json
+from datetime import datetime
+from datetime import timedelta
 
 
 while True:
     try:
-        username = input("Please enter a Github username: ")
-        password = getpass.getpass() #receive the password securely without echoeing back to terminal
-        g = Github(username,password)
-        authen_user = g.get_user(username)
+        #username = input("Please enter a Github username: ")
+    #    password = getpass.getpass() #receive the password securely without echoeing back to terminal
+        g = Github("saphilli","aggrav4247")
+    #    authen_user = g.get_user(username)
     except Exception:
         print("Invalid credentials")
         continue;
     else:
-        print('Logged in as '+username)
+        #print('Logged in as '+username)
         break;
 
-languages = set([])
-repos = []
-user = input("Enter a Github username to view their stats: ")
-#create set of all the repositories languages
-for repo in g.get_user(user).get_repos():
-    print(repo.name , "\n -" ,repo.language)
-    if repo.language != None:
-        languages.add(repo.language)
-    if repo.name != None:
-        repos.append(repo.name)
 
-    #print repo.get_dir_contents(repo.name)
-def writeToJSON(path,fileName,data):
-    filePathName= path+'/'+fileName+'.json'
-    with open(filePathName,'w') as out:
-        json.dump(data,out)
 
-path='./'
-fileName1 = 'languages'
-fileName2 = 'repos'
-writeToJSON(path,fileName1,list(languages))
-writeToJSON(path,fileName2,repos)
+#user = input("Enter a Github username to view their stats: ")
+
+#get a repository
+#print("Below is a list of the user's repositorys")
+#for repo in g.get_user("YasirZardari").get_repos():
+#    print(repo.name )
+
+#repo_name = input("Please enter one of the repository names listed above: ")
+repo = g.get_user("YasirZardari").get_repo("Speech-Therapy-App")
+
+commits = repo.get_commits() #(reverse the list of commits to get first one)
+lastCommitDate = str.split(str(commits[0].commit.author.date))[0] #isolate the date from time
+commits = commits.reversed
+firstCommitDate = str.split(str(commits[0].commit.author.date))[0]
+
+#create an array of commit dates and their authors in chronological order
+commitStats = []
+for c in commits:
+    date = str.split(str(c.commit.author.date)) #split date and time into array
+    name = str(c.commit.author.name)
+    dateAndAuthor = date[0]+" "+ name.replace(" ","")
+    commitStats.append(dateAndAuthor)
+
+def stringToDate(date):
+    splitDate = str.split(date,"-")
+    dateObject = datetime(int(splitDate[0]),int(splitDate[1]),int(splitDate[2]))
+    dateObject =datetime.date(dateObject)
+    return dateObject
+
+currentCommitDate = stringToDate(firstCommitDate)
+lastCommitDate = stringToDate(lastCommitDate)
+
+#create dictionary of contributor names and initialize commit count to 0
+contributors = {}
+for username in repo.get_contributors():
+    contributors[username.login] = 0
+
+def writeToJS(data):
+    with open("./data.js", 'a') as file:
+        file.write(data)
+
+writeToJS("var data = [ \n") #start off the javascript data file
+#construct the js file
+while(currentCommitDate<=lastCommitDate):
+    writeToJS("{\n'day':"+str(currentCommitDate.day)
+    +",\n'month':" +str(currentCommitDate.month)
+    +",\n'year':"+str(currentCommitDate.year) +",\n'values': [\n")
+
+    for i in commitStats:
+        if(stringToDate(i[0:10])==currentCommitDate):
+            contributors[i[11:50]]+=1
+    count = 0
+    for j in contributors:
+        count+=1
+        writeToJS("{'value':"+str(contributors[j])+ ",\n'name':'"+ str(j)+ "'}\n")
+        if(count!=len(contributors)):
+            writeToJS(",")
+        else:
+            writeToJS("]\n}")
+    currentCommitDate += timedelta(days=1)
+    if (currentCommitDate<=lastCommitDate):
+        writeToJS(",")
+    else:
+        writeToJS("]")
